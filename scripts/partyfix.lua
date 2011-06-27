3782e36d607f5c8ee82cb4f064ea6c43710c0027
hix89 <hix89@6627fd71-9ded-4a83-af9a-d10cbad460bc>
2011-06-27 12:59:40 +0000

    scripts/partyfix.lua: - Enhanced error detection to remove messages with oversized prefix or msg types. - Added relevant debug info for debug mode.

--- scripts/partyfix.lua
+++ scripts/partyfix.lua
@@ -1,3 +1,4 @@
+-- Create slash command to enable debug mode.
 local DEBUG = false
 SlashCmdList["YAIAPFIX"] = function()
 	DEBUG = not DEBUG
@@ -5,16 +6,35 @@ SlashCmdList["YAIAPFIX"] = function()
 end
 SLASH_YAIAPFIX1 = "/yaiap"
 
+-- Hook SendAddonMessage.
 local old = SendAddonMessage
 local function fix(pre, msg, ch, ...)
-	local chl = strlower(ch)
-	if (chl == "raid" and GetRealNumRaidMembers() == 0) or (chl == "party" and GetRealNumPartyMembers() == 0) or (chl == "guild" and not IsInGuild()) then
-		if DEBUG == true then 
+	-- Filter messages with oversized parameters.
+	if (#pre > 15) or (#msg > 250) then
+		if DEBUG then
+			-- Print message error info.
 			print("|c00ff0000YAIAP|r: ["..strupper(ch).."] prefix = |c0000ff00"..pre.."|r: debugstack = "..debugstack(3, 4, 0))
+
+			-- Pipe errored message to SendAddonMessage to create an error for debug.
 			old(pre, msg, ch, ...)
 		end
 		return
 	end
+
+	-- Filter messages en route to a channel not accessible.
+	local chl = strlower(ch)
+	if (chl == "raid" and GetRealNumRaidMembers() == 0) or (chl == "party" and GetRealNumPartyMembers() == 0) or (chl == "guild" and not IsInGuild()) then
+		if DEBUG then
+			-- Print message error info.
+			print("|c00ff0000YAIAP|r: ["..strupper(ch).."] prefix = |c0000ff00"..pre.."|r: debugstack = "..debugstack(3, 4, 0))
+
+			-- Pipe errored message to SendAddonMessage to create an erorr for debug.
+			old(pre, msg, ch, ...)
+		end
+		return
+	end
+
+	-- Pipe accepted message to SendAddonMessage.
 	old(pre, msg, ch, ...)
 end
 
