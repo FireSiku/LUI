53e2c948f8af58d157ac9499ec68e43c88e73113
thaly1989 <thaly1989@6627fd71-9ded-4a83-af9a-d10cbad460bc>
2011-07-01 11:08:33 +0000

    - updated oUF vengeance - added firelands stuff to raiddebuff icon

--- unitframes/plugins/oUF_Vengeance.lua
+++ unitframes/plugins/oUF_Vengeance.lua
@@ -1,7 +1,7 @@
 --[[
 	Project.: oUF_Vengeance
 	File....: oUF_Vengeance.lua
-	Version.: 40200.2
+	Version.: 40200.3
 	Rev Date: 06/28/2011
 	Authors.: Shandrela [EU-Baelgun] <Bloodmoon>
 ]]
@@ -43,25 +43,14 @@ local tooltip = CreateFrame("GameTooltip", "VengeanceTooltip", UIParent, "GameTo
 tooltip:SetOwner(UIParent, "ANCHOR_NONE")
 
 local function getTooltipText(...)
-	local text = ""
-	for i=1,select("#",...) do
-		local rgn = select(i,...)
-		if rgn and rgn:GetObjectType() == "FontString" then
-			text = text .. (rgn:GetText() or "")
+	local t = ""
+	for i = 1, select("#",...) do
+		local r = select(i,...)
+		if r and r:GetObjectType() == "FontString" then
+			t = t .. (r:GetText() or "")
 		end
 	end
-	return text
-end
-
-local function maxChanged(self, event, unit)
-	if unit ~= "player" then return end
-	local bar = self.Vengeance
-	
-	local health = UnitHealthMax("player")
-	local _, stamina = UnitStat("player", 3)
-	
-	bar.max = 0.1 * (health - 15 * stamina) + stamina
-	bar:SetMinMaxValues(0, bar.max)
+	return t
 end
 
 local function valueChanged(self, event, unit)
@@ -84,8 +73,11 @@ local function valueChanged(self, event, unit)
 		if value > 0 then
 			if value > bar.max then value = bar.max end
 			
+			if value == bar.value then return end
+			
 			bar:SetMinMaxValues(0, bar.max)
 			bar:SetValue(value)
+			bar.value = value
 			bar:Show()
 			
 			if bar.Text then
@@ -101,6 +93,26 @@ local function valueChanged(self, event, unit)
 	else
 		bar:Hide()
 	end
+end
+
+local function maxChanged(self, event, unit)
+	if unit ~= "player" then return end
+	local bar = self.Vengeance
+	
+	if not bar.isTank then
+		bar:Hide()
+		return
+	end
+	
+	local health = UnitHealthMax("player")
+	local _, stamina = UnitStat("player", 3)
+	
+	if not health or not stamina then return end
+	
+	bar.max = 0.1 * (health - 15 * stamina) + stamina
+	bar:SetMinMaxValues(0, bar.max)
+	
+	valueChanged(self, event, unit)
 end
 
 local function isTank(self, event)
@@ -131,8 +143,7 @@ local function isTank(self, event)
 		bar:Hide()
 	end
 	
-	-- little hack (for a bug i mentioned with my druid -.-)
-	if event == "PLAYER_LOGIN" then bar:Hide() end
+	maxChanged(self, event, unit)
 end
 
 local function Enable(self, unit)
@@ -140,17 +151,14 @@ local function Enable(self, unit)
 	
 	if bar and unit == "player" then
 		bar.max = 0
+		bar.value = 0
 		
 		self:RegisterEvent("UNIT_AURA", valueChanged)
 		
 		self:RegisterEvent("UNIT_MAXHEALTH", maxChanged)
-		self:RegisterEvent("PLAYER_ENTERING_WORLD", maxChanged)
 		self:RegisterEvent("UNIT_LEVEL", maxChanged)
 		
-		self:RegisterEvent("ACTIVE_TALENT_GROUP_CHANGED", isTank)
-		self:RegisterEvent("PLAYER_TALENT_UPDATE", isTank)
-		self:RegisterEvent("PLAYER_LOGIN", isTank)
-		self:RegisterEvent("PLAYER_ALIVE", isTank)
+		self:RegisterEvent("PLAYER_REGEN_DISABLED", isTank)
 		
 		if class == "DRUID" then
 			self:RegisterEvent("UPDATE_SHAPESHIFT_FORM", isTank)
@@ -169,13 +177,9 @@ local function Disable(self)
 		self:UnregisterEvent("UNIT_AURA", valueChanged)
 		
 		self:UnregisterEvent("UNIT_MAXHEALTH", maxChanged)
-		self:UnregisterEvent("PLAYER_ENTERING_WORLD", maxChanged)
 		self:UnregisterEvent("UNIT_LEVEL", maxChanged)
 		
-		self:UnregisterEvent("ACTIVE_TALENT_GROUP_CHANGED", isTank)
-		self:UnregisterEvent("PLAYER_TALENT_UPDATE", isTank)
-		self:UnregisterEvent("PLAYER_LOGIN", isTank)
-		self:UnregisterEvent("PLAYER_ALIVE", isTank)
+		self:UnregisterEvent("PLAYER_REGEN_DISABLED", isTank)
 		
 		if class == "DRUID" then
 			self:UnregisterEvent("UPDATE_SHAPESHIFT_FORM", isTank)
