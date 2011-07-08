324f43dff4f54bba3f0a9bf6a4a5a58693fd0b39
hix89 <hix89@6627fd71-9ded-4a83-af9a-d10cbad460bc>
2011-07-08 16:22:09 +0000

    scripts/error.lua: - Added small hook to default Error frame which adds LUI version and revision info to error messages (since many people don't include this info for us).

--- scripts/error.lua
+++ scripts/error.lua
@@ -22,3 +22,18 @@ enable:SetScript("OnEvent", function(self)
 		hideErrors()
 	end
 end)
+
+-- Hook error frame so that error messages contain LUI version and revision info.
+local frame = CreateFrame("Frame")
+frame:RegisterEvent("ADDON_LOADED")
+
+frame:SetScript("OnEvent", function(self, event, addon)
+	if not (addon == "Blizzard_DebugTools") then return end
+
+	-- Hook the :SetText function.
+	local old = ScriptErrorsFrameScrollFrameText.SetText
+	ScriptErrorsFrameScrollFrameText.SetText = function (self, text)
+		local new = ("|cffffd200LUI Version:|cffffffff "..GetAddOnMetadata("LUI", "Version").." ("..GetAddOnMetadata("LUI", "X-Curse-Packaged-Version")..")\n")..text
+		old(self, new)
+	end
+end)
