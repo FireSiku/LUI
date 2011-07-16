7e25f0b583b4b651f02351438d916e462df9a190
hix89 <hix89@6627fd71-9ded-4a83-af9a-d10cbad460bc>
2011-07-16 17:41:02 +0000

    scripts/errorframe.lua: - The LUI only errors is difficult. I've changed it so that it checks the full text for a LUI file, rather than the file the error appeared in. Although generally errors are from the files they are in, LUI does hook and taint many other files; so it may not be a direct error, but could be in the call stack.

--- trunk/scripts/errorframe.lua
+++ trunk/scripts/errorframe.lua
@@ -8,7 +8,7 @@ script:RegisterEvent("ADDON_LOADED", function(event, addon)
 
 	-- Hook the :SetText function.
 	script:RawHook(ScriptErrorsFrameScrollFrameText, "SetText", function(frame, text)
-		if strfind(strsub(text, 1, strfind(text, "\n")), "Interface\\AddOns\\LUI") then
+		if strfind(text, "Interface\\AddOns\\LUI") then
 			text = ("|cffffd200LUI Version:|cffffffff "..GetAddOnMetadata("LUI", "Version").." ("..GetAddOnMetadata("LUI", "X-Curse-Packaged-Version")..")\n")..text
 		end
 		script.hooks[frame].SetText(frame, text)
