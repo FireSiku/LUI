db440d42649d2afcd84054ef58cbdc07deaa3283
thaly1989 <thaly1989@6627fd71-9ded-4a83-af9a-d10cbad460bc>
2011-08-22 12:48:19 +0000

    - unitframes/layout.lua fixed a bug with spawning unitframes with unitframes disabled
    - modules/bars.lua
    fixed some tiny bugs
    some cosmetic stuff for the options <3
    readded the red buttons for out of range (don't know why i deleted it oO)

--- trunk/unitframes/layout.lua
+++ trunk/unitframes/layout.lua
@@ -3346,6 +3346,8 @@ end
 -- "subframes" of groups, like party target are included within the party toggle
 -- has to be OnEnable
 function module:OnEnable()
+	if db.oUF.Settings.Enable ~= true then return end
+	
 	-- remove with LUI v3.6 or something like that!
 	if LUICONFIG.Versions.ouf ~= LUI.Versions.ouf then	
 		for _, oufdb in pairs(db.oUF) do
