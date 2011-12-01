a9f56ef6edb079f2c0ce84a229bffcc9507f6abe
deadlyslaya <deadlyslaya@6627fd71-9ded-4a83-af9a-d10cbad460bc>
2011-12-01 08:44:23 +0000

    Toc Bump

--- tags/3.6.5b/init.lua
+++
@@ -1,6 +0,0 @@
-local addonname, addon = ...
-local LUI = LibStub("AceAddon-3.0"):NewAddon(addon, addonname)
-LUI.L = LibStub("AceLocale-3.0"):GetLocale(addonname)
-
-SLASH_RELOADUI1 = "/rl"
-SlashCmdList.RELOADUI = ReloadUI
