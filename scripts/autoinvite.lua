cf4c0f8108a3d499716a4aeb6650951fa92ce9b4
hix89 <hix89@6627fd71-9ded-4a83-af9a-d10cbad460bc>
2012-01-09 14:09:19 +0000

    autoinvite.lua: - Clean up script; removed redundant checks and made popup accept smaller. - Auto accept now accepts party invites from BN friends from different realms (cross-realm parties).

--- scripts/autoinvite.lua
+++ scripts/autoinvite.lua
@@ -2,36 +2,31 @@ local addonname, LUI = ...
 local script = LUI:NewScript("AutoInvite", "AceEvent-3.0")
 
 local function isFriend(name)
-	for i=1, GetNumFriends() do
-		if strlower(GetFriendInfo(i)) == strlower(name) then
+	for i = 1, GetNumFriends() do
+		if GetFriendInfo(i) == name then
 			return true
 		end
 	end
 end
 
 local function isGuildmate(name)
-	if IsInGuild() then
-		for i=1, GetNumGuildMembers() do
-			if strlower(GetGuildRosterInfo(i)) == strlower(name) then
-				return true
-			end
+	if not IsInGuild() then return end
+
+	for i = 1, GetNumGuildMembers() do
+		if GetGuildRosterInfo(i) == name then
+			return true
 		end
 	end
 end
 
 local function isBNFriend(name)
-	if BNFeaturesEnabledAndConnected() then
-		local playerRealm = GetRealmName()
-		for i=1, BNGetNumFriends() do
-			pID, _, _, _, _, client, isOnline = BNGetFriendInfo(i)
-			if client == "WoW" and isOnline then
-				_, tName, _, realm = BNGetToonInfo(pID)
-				if realm == playerRealm then
-					if strlower(tName) == strlower(name) then
-						return true
-					end
-				end
-			end
+	if not BNFeaturesEnabledAndConnected() then return end
+
+	for i = 1, BNGetNumFriends() do
+		local pID, _, _, _, _, client, isOnline = BNGetFriendInfo(i)
+		if isOnline and client == "WoW" then
+			local _, tName, = BNGetToonInfo(pID)
+			if tName == name then return true end
 		end
 	end
 end
@@ -56,18 +51,15 @@ end
 
 
 function script:PARTY_INVITE_REQUEST(event, sender)
-	if sender == nil then return end
+	if not sender then return end
 
 	if isFriend(sender) or isGuildmate(sender) or isBNFriend(sender) then
-		AcceptGroup()
-		for i=1, STATICPOPUP_NUMDIALOGS do
-			local dlg = _G["StaticPopup"..i]
-			if dlg.which == "PARTY_INVITE" then
-				dlg.inviteAccepted = 1
-				break
+		for i = 1, STATICPOPUP_NUMDIALOGS do
+			local popup = _G["StaticPopup"..i]
+			if popup.which == "PARTY_INVITE" then
+				return popup.button1:GetScript("OnClick")(popup.button1)
 			end
 		end
-		StaticPopup_Hide("PARTY_INVITE")
 	end
 end
 
