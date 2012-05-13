cc4371effd7049bcc452207e1614b1592e5969e1
deadlyslaya <deadlyslaya@6627fd71-9ded-4a83-af9a-d10cbad460bc>
2012-05-13 10:26:23 +0000

    Hopefully fixed bug with Locale namespaces...again

--- trunk/locale/deDE.lua
+++ trunk/locale/deDE.lua
@@ -3,4 +3,17 @@ local addonname, LUI = ...
 local L = _G.LibStub("AceLocale-3.0"):NewLocale(addonname, "deDE")
 if not L then return end
 
---@localization(locale="deDE", format="lua_additive_table", handle-unlocalized="comment", handle-subnamespaces="none", same-key-is-true=true)@
+-- Core
+--@localization(locale="deDE", format="lua_additive_table", handle-unlocalized="comment", namespace="", handle-subnamespaces="none", same-key-is-true=true)@
+
+
+-- Chat
+--@localization(locale="deDE", format="lua_additive_table", handle-unlocalized="comment", namespace="Chat", handle-subnamespaces="none", same-key-is-true=true)@
+
+
+-- WorldMap
+--@localization(locale="deDE", format="lua_additive_table", handle-unlocalized="comment", namespace="WorldMap", handle-subnamespaces="none", same-key-is-true=true)@
+
+
+-- UnitFrames
+--@localization(locale="deDE", format="lua_additive_table", handle-unlocalized="comment", namespace="Unitframes", handle-subnamespaces="none", same-key-is-true=true)@
--- trunk/locale/enUS.lua
+++ trunk/locale/enUS.lua
@@ -7,4 +7,18 @@ silent = true
 
 local L = _G.LibStub("AceLocale-3.0"):NewLocale(addonname, "enUS", true, silent)
 
---@localization(locale="deDE", format="lua_additive_table", handle-unlocalized="comment", handle-subnamespaces="none", same-key-is-true=true)@
+
+-- Core
+--@localization(locale="enUS", format="lua_additive_table", handle-unlocalized="comment", namespace="", handle-subnamespaces="none", same-key-is-true=true)@
+
+
+-- Chat
+--@localization(locale="enUS", format="lua_additive_table", handle-unlocalized="comment", namespace="Chat", handle-subnamespaces="none", same-key-is-true=true)@
+
+
+-- WorldMap
+--@localization(locale="enUS", format="lua_additive_table", handle-unlocalized="comment", namespace="WorldMap", handle-subnamespaces="none", same-key-is-true=true)@
+
+
+-- UnitFrames
+--@localization(locale="enUS", format="lua_additive_table", handle-unlocalized="comment", namespace="Unitframes", handle-subnamespaces="none", same-key-is-true=true)@
