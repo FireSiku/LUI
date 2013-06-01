a771890c0507ddc381235bdb9558c7327c1e769f
kitkeh <kitkeh@6627fd71-9ded-4a83-af9a-d10cbad460bc>
2013-06-01 01:16:03 +0000

    - Unitframes: add commented out "Class" color section with the real Blizzard colors. Might be used in a future option somewhere. Some have asked for it - but they don't seem to look that good with LUI (imo). - Unitframes: small change to fix fade others debuffs and debuff frame color.

--- unitframes/layout/layout.lua
+++ unitframes/layout/layout.lua
@@ -809,10 +809,11 @@ local PostCreateAura = function(element, button)
 	button.auratype:SetTexCoord(0, 1, 0.02, 1)
 end
 
-local PostUpdateAura = function(icons, unit, icon, index, offset, filter, isDebuff, duration, timeLeft)
+--local PostUpdateAura = function(icons, unit, icon, index, offset, filter, isDebuff, duration, timeLeft) - leaving here just in case I need to revert it
+local PostUpdateAura = function(icons, unit, icon, index, offset)
 	local _, _, _, _, dtype, duration, expirationTime, unitCaster, _ = UnitAura(unit, index, icon.filter)
 	if not (unitCaster == "player" or unitCaster == "pet" or unitCaster == "vehicle") then
-		if icon.debuff then
+		if icon.isDebuff then
 			icon.icon:SetDesaturated(icons.fadeOthers)
 		end
 	end
