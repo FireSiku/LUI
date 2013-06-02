b85497e16e77e57032182e6cb41722c5e6149fca
kitkeh <kitkeh@6627fd71-9ded-4a83-af9a-d10cbad460bc>
2013-06-02 11:49:25 +0000

    - Talent Spam: Update filters to include Pet talent spam.

--- trunk/scripts/talentspam.lua
+++ trunk/scripts/talentspam.lua
@@ -5,9 +5,13 @@ local spam1 = gsub(ERR_LEARN_ABILITY_S:gsub("%.", "%."), "%%s", "(.*)")
 local spam2 = gsub(ERR_LEARN_SPELL_S:gsub("%.", "%."), "%%s", "(.*)")
 local spam3 = gsub(ERR_SPELL_UNLEARNED_S:gsub("%.", "%."), "%%s", "(.*)")
 local spam4 = gsub(ERR_LEARN_PASSIVE_S:gsub("%.", "%."), "%%s", "(.*)")
+local spam5 = gsub(ERR_PET_LEARN_ABILITY_S:gsub("%.", "%."), "%%s", "(.*)")
+local spam6 = gsub(ERR_PET_LEARN_SPELL_S:gsub("%.", "%."), "%%s", "(.*)")
+local spam7 = gsub(ERR_PET_SPELL_UNLEARNED_S:gsub("%.", "%."), "%%s", "(.*)")
 
 local function spamFilter(self, event, msg)
-	if strfind(msg, spam1) or strfind(msg, spam2) or strfind(msg, spam3) or strfind(msg, spam4) then return true end
+	if strfind(msg, spam1) or strfind(msg, spam2) or strfind(msg, spam3) or strfind(msg, spam4)
+		or strfind(msg, spam5) or strfind(msg, spam6) or strfind(msg, spam7) then return true end
 end
 
 function script:AddFilter(...)
@@ -27,8 +31,10 @@ end
 function script:SetTalentSpam()
 	if LUI.db.profile.General.HideTalentSpam then
 		self:SecureHook("SetActiveSpecGroup", "AddFilter")
+		self:SecureHook("SetSpecialization", "AddFilter")
 	else
 		self:Unhook("SetActiveSpecGroup")
+		self:Unhook("SetSpecialization")
 	end
 end
 
