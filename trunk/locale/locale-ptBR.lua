fe91494365c70597af55cdcb96f28f8e9b0232d0
chaoslux <chaoslux@6627fd71-9ded-4a83-af9a-d10cbad460bc>
2014-10-26 21:17:13 +0000

    - The localization is still awfully half-finished, but the work that has already been done on it shouldn't go to waste. This should hopefully bring back all localizations to LUI.

---
+++ trunk/locale/locale-ptBR.lua
@@ -0,0 +1,5 @@
+local addonname, LUI = ...
+local L = LibStub("AceLocale-3.0"):NewLocale(addonname, "ptBR")
+if not L then return end
+
+--@localization(locale="ptBR", format="lua_additive_table", handle-unlocalized="english", same-key-is-true=true)@
