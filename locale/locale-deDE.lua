e7311a04038ccf4e4aa2684afd8e09cb66cf8f53
chaoslux <chaoslux@gmail.com>
2014-10-26 21:17:13 +0000

    - The localization is still awfully half-finished, but the work that has already been done on it shouldn't go to waste. This should hopefully bring back all localizations to LUI.

---
+++ locale/locale-deDE.lua
@@ -0,0 +1,5 @@
+local addonname, LUI = ...
+local L = LibStub("AceLocale-3.0"):NewLocale(addonname, "deDE")
+if not L then return end
+
+--@localization(locale="deDE", format="lua_additive_table", handle-unlocalized="english", same-key-is-true=true)@
