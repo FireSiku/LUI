a4d4aa7dfe2759d87eb683d233724499c6f1b509
chaoslux <chaoslux@gmail.com>
2016-07-19 17:52:02 +0000

    - Fixed a "UnitIsTapped" error I missed last night.

--- unitframes/layout/layout.lua
+++ unitframes/layout/layout.lua
@@ -454,7 +454,7 @@ local OverrideHealth = function(self, event, unit, powerType)
 		end
 	end
 
-	if health.colorTapping and UnitIsTapped(unit) and not UnitIsTappedByPlayer(unit) then health:SetStatusBarColor(unpack(module.db.Colors.Misc["Tapped"])) end
+	if health.colorTapping and UnitIsTapped and UnitIsTapped(unit) and not UnitIsTappedByPlayer(unit) then health:SetStatusBarColor(unpack(module.db.Colors.Misc["Tapped"])) end
 
 	local r_, g_, b_ = health:GetStatusBarColor()
 	local mu = health.bg.multiplier or 1
