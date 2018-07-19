bbf3d8803ad3f876fd792eda36314cc7e5bedf42
chaoslux <chaoslux@gmail.com>
2018-07-19 16:14:36 +0000

    - Fixed issues when "Hide Blizzard Errors" is checked.

--- scripts/errorhider.lua
+++ scripts/errorhider.lua
@@ -4,32 +4,32 @@ local script = LUI:NewScript("ErrorHider", "AceEvent-3.0")
 function script:ErrorMessageHandler()
 	if LUI.db.profile.General.HideErrors then
 
-		local function isException(message)
-			local ex = LUI.db.profile.General.ErrorExceptions
-			local message = message
+		-- local function isException(message)
+		-- 	local ex = LUI.db.profile.General.ErrorExceptions
+		-- 	local message = message
 
-			if ex == nil or ex == "" then
-				return false
-			else
-				ex = strlower(ex)
-				message = strlower(message)
+		-- 	if ex == nil or ex == "" then
+		-- 		return false
+		-- 	else
+		-- 		ex = strlower(ex)
+		-- 		message = strlower(message)
 
-				if ex == message or strfind(ex, message..",") or strfind(ex, ","..message) then
-					return true
-				else
-					return false
-				end
-			end
-		end
+		-- 		if ex == message or strfind(ex, message..",") or strfind(ex, ","..message) then
+		-- 			return true
+		-- 		else
+		-- 			return false
+		-- 		end
+		-- 	end
+		-- end
 
 		UIErrorsFrame:UnregisterEvent("UI_ERROR_MESSAGE")
-		self:RegisterEvent("UI_ERROR_MESSAGE", function(event, message)
-			if isException(message) then
-				UIErrorsFrame:AddMessage(message, 1, .1, .1)
-			end
-		end)
+		-- self:RegisterEvent("UI_ERROR_MESSAGE", function(event, message)
+		-- 	if isException(message) then
+		-- 		UIErrorsFrame:AddMessage(message, 1, .1, .1)
+		-- 	end
+		-- end)
 	else
-		self:UnregisterEvent("UI_ERROR_MESSAGE")
+		--self:UnregisterEvent("UI_ERROR_MESSAGE")
 		UIErrorsFrame:RegisterEvent("UI_ERROR_MESSAGE")
 	end
 end
