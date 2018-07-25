d7fee47b81b47266f418f4382f274e7560480f94
chaoslux <chaoslux@6627fd71-9ded-4a83-af9a-d10cbad460bc>
2018-07-25 03:05:48 +0000

    WorldMap: Emissary graphic and Point of Interest filter will now appear again.

--- trunk/scripts/WorldMapFix.lua
+++ trunk/scripts/WorldMapFix.lua
@@ -2,3 +2,10 @@ local addonname, LUI = ...
 local script = LUI:NewScript("WorldMapFix")
 
 WorldMapFrame:SetFrameStrata("HIGH")
+local children = {WorldMapFrame:GetChildren()}
+for k, v in pairs(children) do
+    if v.TrackerBackground or v.Icon then
+        --WorldMapBountyBoardMixin
+        v:SetFrameStrata("DIALOG")
+    end
+end
