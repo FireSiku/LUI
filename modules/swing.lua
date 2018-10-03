2c2eb46188d0989c576d0d9da5eb97472bf391f2
kitkeh <kitkeh@6627fd71-9ded-4a83-af9a-d10cbad460bc>
2018-10-03 08:39:33 +0000

    Swing Timer: hopefully fix the swing timer.

--- modules/swing.lua
+++ modules/swing.lua
@@ -440,23 +440,23 @@ local SetSwing = function()
 	LUISwing.Offhand.Text:SetShadowOffset(1.25, -1.25)
 	LUISwing.Offhand.Text:SetPoint("TOP", LUISwing.Twohand.Text, "CENTER", 0, -1)
 
-	LUISwing:SetScript("OnEvent", function(self, event, ...)
+    LUISwing:SetScript("OnEvent", function(self, event, ...)
 		if event == "PLAYER_REGEN_DISABLED" then
 			if self.Testmode then
 				self.Testmode = nil
 				self:Hide()
 			end
 		elseif event == "UNIT_SPELLCAST_SUCCEEDED" then
-			Ranged(self, event, ...)
+			Ranged(self, event, CombatLogGetCurrentEventInfo())
 		elseif event == "UNIT_RANGEDDAMAGE" then
-			RangedChange(self, event, ...)
+			RangedChange(self, event, CombatLogGetCurrentEventInfo())
 		elseif event == "COMBAT_LOG_EVENT_UNFILTERED" then
-			Melee(self, event, ...)
-			ParryHaste(self, event, ...)
+			Melee(self, event, CombatLogGetCurrentEventInfo())
+			ParryHaste(self, event, CombatLogGetCurrentEventInfo())
 		elseif event == "UNIT_ATTACK_SPEED" then
-			MeleeChange(self, event, ...)
+			MeleeChange(self, event, CombatLogGetCurrentEventInfo())
 		elseif event == "PLAYER_REGEN_ENABLED" then
-			Ooc(self, event, ...)
+			Ooc(self, event, CombatLogGetCurrentEventInfo())
 		end
 	end)
 end
