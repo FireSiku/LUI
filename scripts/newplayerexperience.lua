ba22e5513de15a89f1ada5cd5e8463530813fafb
FireSiku <chaoslux@gmail.com>
2020-11-01 05:30:59 -0500

    Script: Finished NewPlayerExperience script. - All of Exile Reach should be working fine now. - Supports LUI Bars and BT4 until they add their own support for it. - Correctly detect if Spellbook notification should be shown. - Do not show LFG tutorial while you're inside the dungeon.

--- scripts/newplayerexperience.lua
+++ scripts/newplayerexperience.lua
@@ -21,26 +21,181 @@ local function isUsingLUIBars()
 	end 
 end
 
+local TutorialData = {}
+TutorialData.LevelAbilitiesTable = {
+	WARRIOR = {
+		1464,	-- Start with Slam
+		100,	-- Charge, level 2
+		23922,	-- Shield Slam, level 3
+		1715,	-- Hamstring, level 4
+		34428,	-- Victory Rush, level 5
+		2565,	-- Shield Block, level 6
+		6552,	-- Pummel, level 7
+		nil,	-- 319157, Charge Rank 2, level 8
+		1680,	-- Whirlwind, level 9
+	};
+	PALADIN = {
+		35395,	-- Start with Crusader Strike
+		53600,	-- Shield of the Righteous, level 2
+		20271,	-- Judgment, level 3
+		19750,	-- Flash of Light, level 4
+		853,	-- Hammer of Justice, level 5
+		26573,	-- Consecration, level 6
+		85673,	-- Word of Glory, level 7
+		nil,	-- 327977, Judgement Rank 2, level 8
+		633,	-- Lay on Hands, level 8
+	};
+	HUNTER = {
+		56641,	-- Start with Steady Shot
+		185358,	-- Arcane Shot, level 2
+		195645,	-- Wing Clip, level 3
+		781,	-- Disengage, level 4
+		186257,	-- Aspect of the Cheetah, level 5
+		5384,	-- Feign Death, level 6
+		257284,	-- Hunter's Mark, level 7
+		186265,	-- Aspect of the Turtle, level 8
+		109304,	-- Exhilaration, level 9
+	};
+	ROGUE = {
+		1752,	-- Start with Sinister Strike
+		196819,	-- Eviscerate, level 2
+		nil,	-- 1833 Cheap Shot, level 3 - Rogues also get Stealth, 1784
+		nil,	-- 
+		2983,	-- Sq, level 5
+		1766,	-- Kick, level 6
+		nil,	-- 8676, Ambush, level 7
+		185311,	-- Crimson Vial, level 8
+		315496,	-- Slice and Dice, level 9
+	};
+	PRIEST = {
+		585,	-- Start with Smite
+		589,	-- Shadow Word: Pain, level 2
+		2061,	-- Flash Heal, level 3
+		17,		-- Power Word: Shield, level 4
+		8092,	-- Mind Blast, level 5
+		21562,	-- Power Word: Fortitude, level 6
+		8122,	-- Psychic Scream, level 7
+		19236,	-- Desperate Prayer, level 8
+		586,	-- Fade, level 9
+	};
+	SHAMAN = {
+		188196,	-- Start with Lightning Bolt
+		73899,	-- Primal Strike, level 2
+		188389,	-- Flame Shock, level 3
+		8004,	-- Healing Surge, level 4
+		2484,	-- Earthbind Totem, level 5
+		nil,	-- 318044,	Lightning Bolt Rank 2, level 6
+		318038,	-- Flametongue Weapon, level 7
+		nil,	-- 20608,-- Reincarnation, level 8
+		192106,	-- Lightning Shield, level 9
+	};
+	MAGE = {
+		116,	-- Start with Frostbolt
+		319836,	-- Fire Blast, level 2
+		122,	-- Frost Nova, level 3
+		1953,	-- Blink, level 4
+		190336,	-- Conjure Refreshment, level 5
+		1449,	-- Arcane Explosion, level 6
+		2139,	-- Counterspell, level 7
+		1459,	-- Arcane Intellect, level 8
+		130,	-- Slow Fall, level 9
+	};
+	WARLOCK = {
+		686,	-- Start with Shadow Bolt
+		172,	-- Corruption, level 2
+		688,	-- Summon Imp, level 3
+		104773,	-- Unending Resolve, level 4
+		5782,	-- Fear, level 5
+		702,	-- Curse of Weakness, level 6
+		6201,	-- Create Healthstone, level 7
+		755,	-- Health Funnel, level 8
+		234153,	-- Drain Life, level 9
+	};
+	MONK = {
+		100780,	-- Start with Tiger Palm
+		100784,	-- Blackout Kick, level 2
+		109132,	-- Roll, level 3
+		116670,	-- Vivify, level 4
+		117952,	-- Crackling Jade Lightning, level 5
+		119381,	-- Leg Sweep, level 6
+		101546,	-- Spinning Crane Kick, level 7
+		322101,	-- Expel Harm, level 8
+		nil,	-- 328669, Roll Rank 2, level 9
+	};
+	DRUID = {
+		5176,	-- Start with Wrath
+		8921,	-- Moonfire, level 2
+		8936,	-- Regrowth, level 3
+		339,	-- Entangling Roots, level 4
+		nil,	-- 5221, Shred, level 5
+		1850,	-- Dash, level 6
+		nil,	-- 22568, Ferocious Bite, level 7
+		nil,	-- 33917, Mangle, level 8
+		nil,	-- 326646, Moonfire Rank 2, level 9
+	};
+}
+
 -- Required for tutorial to let you point to abilities.
 function script:GetActionButtonBySpellID(helper, spellID)
-	if not isUsingLUIBars() or (type(spellID) ~= "number") then return end
-	for i = 1, 12 do
-		local btn = _G["LUIBarBottom1Button"..i]
-		if btn and btn:IsVisible() then
-			local actionType, sID, subType = GetActionInfo(btn.action)
-			if (sID == spellID) then
-				return btn
-			elseif (actionType == "flyout" and FlyoutHasSpell(sID, spellID)) then
-				return btn
+	if (type(spellID) ~= "number") then return end
+	if self.BT4 then
+		-- Bartender4 path, until this feature has been added in BT4 proper.
+		for i = 1, 24 do
+			local btn = _G["BT4Button"..i]
+			if btn and btn:IsVisible() then
+				local actionType, sID, subType = GetActionInfo(btn:CalculateAction())
+				if (sID == spellID) then
+					return btn
+				elseif (actionType == "flyout" and FlyoutHasSpell(sID, spellID)) then
+					return btn
+				end
+			end
+		end
+	else
+		-- LUI Bars
+		for i = 1, 12 do
+			local btn = _G["LUIBarBottom1Button"..i]
+			if btn and btn:IsVisible() then
+				local actionType, sID, subType = GetActionInfo(btn.action)
+				if (sID == spellID) then
+					return btn
+				elseif (actionType == "flyout" and FlyoutHasSpell(sID, spellID)) then
+					return btn
+				end
 			end
 		end
 	end
 	-- backup for stance bars
 	for i = 1, 10 do
-		local btn = _G["StanceButton" .. i]
+		local btn = self.BT4 and _G["BT4StanceButton"..i] or _G["StanceButton"..i]
 		local icon, isActive, isCastable, sID = GetShapeshiftFormInfo(btn:GetID())
 		if (sID == spellID) then
 			return btn
+		end
+	end
+end
+
+function script:FindEmptyButton()
+	if self.BT4 then
+		for i = 1, 24 do
+			local btn = _G["BT4Button"..i]
+			if btn then
+				local _, sID = GetActionInfo(btn:CalculateAction())
+				if not sID then
+					return btn;
+				end
+			end
+		end
+	else
+		-- LUI Bars
+		for i = 1, 12 do
+			local btn = _G["LUIBarBottom1Button"..i]
+			if btn then
+				local _, sID = GetActionInfo(btn.action)
+				if not sID then
+					return btn;
+				end
+			end
 		end
 	end
 end
@@ -57,8 +212,6 @@ function script:SetTutorialFrames()
 
 	script:SecureHook(NPE_TutorialWalk_Frame, "ShowTutorial", moveAway)
 	moveAway(NPE_TutorialWalk_Frame)
-
-	--script:SecureHook(NPE_TutorialSingleKey_Frame, "ShowTutorial", moveAway)
 end
 
 local arrowDirections = {"UP", "LEFT", "RIGHT", "DOWN"}
@@ -80,7 +233,7 @@ function script:ShowPointerArrow(frame, direction)
 	local arrow2 = frame["Arrow_"..direction..2]
 	local point = (direction == "UP") and "TOP" or (direction == "DOWN") and "BOTTOM" or direction
 	arrow1:ClearAllPoints()
-	-- arrow2:ClearAllPoints()
+	arrow2:ClearAllPoints()
 	local offsetX = (direction == "UP" or direction == "DOWN") and 0 or NegateIf(25, direction == "RIGHT")
 	local offsetY = (direction == "LEFT" or direction == "RIGHT") and 0 or NegateIf(25, direction == "DOWN")
 	arrow1:SetPoint(LUI.Opposites[point], frame, point, offsetX, offsetY)
@@ -97,11 +250,29 @@ end
 function script:ShouldReAnchorPointer(frame)
 	local text = frame.Content.Text:GetText()
 	local buttons, anchor = LUI.MicroMenu.Buttons, nil
-	--LUI:Print(text)
+	if not frame:IsShown() and text then frame:Show() end
 	if text == NPEV2_SPELLBOOK_TUTORIAL then
 		anchor = buttons.Spellbook
+		-- HACK: When using BT4, Spellbook warning would always appear on reload.-
+		if self.BT4 and not script:ShowSpellbook() then
+			frame:Hide()
+			-- This function isn't always called on level up, make sure to show it if needed.
+			script:RegisterEvent("PLAYER_LEVEL_CHANGED", function()
+				script:ShouldReAnchorPointer(frame)
+			end)
+			return
+		end
 	elseif text == format(NPEV2_SHOW_BAGS, TutorialHelper:GetBagBinding()) then
 		anchor = buttons.Bags
+	elseif text == NPEV2_LFD_INTRO then
+		anchor = buttons.LFG
+		-- Make sure warning is only visible while you're on Exile Reach
+		if WorldMapFrame:GetMapID() ~= 1409 then
+			frame:Hide()
+			return
+		end
+	elseif text == NPEV2_MOUNT_TUTORIAL_P2_NEW_MOUNT_ADDED then
+		anchor = buttons.Pets
 	else
 		return
 	end
@@ -117,20 +288,39 @@ function script:SetPointerFrame()
 		local newPointer = anchorFrame.currentNPEPointer
 		script:ShouldReAnchorPointer(newPointer)
 	end)
-	if NPE_PointerFrame_1 then
-		script:ShouldReAnchorPointer(NPE_PointerFrame_1)
+	-- If the frames already exist, hook will not be called so we have to check manually.
+	for i = 1, 3 do
+		local frame = _G["NPE_PointerFrame_"..i]
+		if frame then
+			script:ShouldReAnchorPointer(frame)
+		end
+	end
+end
+
+function script:ShowSpellbook()
+	local LevelUpTutorial_spellIDlookUp = TutorialHelper:FilterByClass(TutorialData.LevelAbilitiesTable);
+	local playerLevel = UnitLevel("player");
+	for startLevel = 1, playerLevel do
+		local spellID = LevelUpTutorial_spellIDlookUp[startLevel];
+
+		local button = TutorialHelper:GetActionButtonBySpellID(spellID);
+		if not button then
+			return true
+		end
 	end
 end
 
 function script:ADDON_LOADED(event, addon)
 	if addon == "Blizzard_NewPlayerExperience" then
-		script:RawHook(TutorialHelper, "GetActionButtonBySpellID", "GetActionButtonBySpellID", true)
+		if IsAddOnLoaded("Bartender4") then self.BT4 = true end
+		if isUsingLUIBars() or self.BT4 then
+			script:RawHook(TutorialHelper, "GetActionButtonBySpellID", "GetActionButtonBySpellID", true)
+			script:RawHook(TutorialHelper, "FindEmptyButton", "FindEmptyButton", true)
+		end
+		
 		script:SetTutorialFrames()
 		script:SetPointerFrame()
 	end
 end
-
--- Spellbook: NPEV2_SPELLBOOK_TUTORIAL, NPEV2_CALL_QUEST_ABILITY_TUTORIAL??, 
--- Bags: TUTORIAL58, TUTORIAL59
 
 script:RegisterEvent("ADDON_LOADED")
