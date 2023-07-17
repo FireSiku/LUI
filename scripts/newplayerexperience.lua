bb8abe1c7ed364402a96a896718f77e577cb7602
FireSiku <chaoslux@gmail.com>
2023-07-17 12:15:15 -0400

    UIElements: Removed the NPE pointer arrow code as it is now part of uiAlerts

--- scripts/newplayerexperience.lua
+++ scripts/newplayerexperience.lua
@@ -1,7 +1,6 @@
 ---@class LUIAddon
 local LUI = select(2, ...)
 local script = LUI:NewScript("NewPlayerExp", "AceEvent-3.0", "AceHook-3.0")
-local Micromenu = LUI:GetModule("Micromenu", true) --[[@as LUI.Micromenu]]
 
 local function S(x) return LUI:Scale(x) end
 
@@ -9,121 +8,6 @@ local GetShapeshiftFormInfo = _G.GetShapeshiftFormInfo
 local FlyoutHasSpell = _G.FlyoutHasSpell
 local IsAddOnLoaded = _G.IsAddOnLoaded
 local GetActionInfo = _G.GetActionInfo
-local UnitLevel = _G.UnitLevel
-
-local TutorialData = {}
-TutorialData.LevelAbilitiesTable = {
-	WARRIOR = {
-		1464,	-- Start with Slam
-		100,	-- Charge, level 2
-		23922,	-- Shield Slam, level 3
-		1715,	-- Hamstring, level 4
-		34428,	-- Victory Rush, level 5
-		2565,	-- Shield Block, level 6
-		6552,	-- Pummel, level 7
-		nil,	-- 319157, Charge Rank 2, level 8
-		1680,	-- Whirlwind, level 9
-	};
-	PALADIN = {
-		35395,	-- Start with Crusader Strike
-		53600,	-- Shield of the Righteous, level 2
-		20271,	-- Judgment, level 3
-		19750,	-- Flash of Light, level 4
-		853,	-- Hammer of Justice, level 5
-		26573,	-- Consecration, level 6
-		85673,	-- Word of Glory, level 7
-		nil,	-- 327977, Judgement Rank 2, level 8
-		62124,	-- Hand of Reckoning, level 9
-	};
-	HUNTER = {
-		56641,	-- Start with Steady Shot
-		185358,	-- Arcane Shot, level 2
-		195645,	-- Wing Clip, level 3
-		781,	-- Disengage, level 4
-		186257,	-- Aspect of the Cheetah, level 5
-		5384,	-- Feign Death, level 6
-		257284,	-- Hunter's Mark, level 7
-		186265,	-- Aspect of the Turtle, level 8
-		109304,	-- Exhilaration, level 9
-	};
-	ROGUE = {
-		1752,	-- Start with Sinister Strike
-		196819,	-- Eviscerate, level 2
-		nil,	-- 1833 Cheap Shot, level 3 - Rogues also get Stealth, 1784
-		nil,	-- 
-		2983,	-- Sprint, level 5
-		1766,	-- Kick, level 6
-		nil,	-- 8676, Ambush, level 7
-		185311,	-- Crimson Vial, level 8
-		315496,	-- Slice and Dice, level 9
-	};
-	PRIEST = {
-		585,	-- Start with Smite
-		589,	-- Shadow Word: Pain, level 2
-		2061,	-- Flash Heal, level 3
-		17,		-- Power Word: Shield, level 4
-		8092,	-- Mind Blast, level 5
-		21562,	-- Power Word: Fortitude, level 6
-		8122,	-- Psychic Scream, level 7
-		19236,	-- Desperate Prayer, level 8
-		586,	-- Fade, level 9
-	};
-	SHAMAN = {
-		188196,	-- Start with Lightning Bolt
-		73899,	-- Primal Strike, level 2
-		188389,	-- Flame Shock, level 3
-		8004,	-- Healing Surge, level 4
-		2484,	-- Earthbind Totem, level 5
-		nil,	-- 318044,	Lightning Bolt Rank 2, level 6
-		318038,	-- Flametongue Weapon, level 7
-		nil,	-- 20608,-- Reincarnation, level 8
-		192106,	-- Lightning Shield, level 9
-	};
-	MAGE = {
-		116,	-- Start with Frostbolt
-		319836,	-- Fire Blast, level 2
-		122,	-- Frost Nova, level 3
-		1953,	-- Blink, level 4
-		190336,	-- Conjure Refreshment, level 5
-		1449,	-- Arcane Explosion, level 6
-		2139,	-- Counterspell, level 7
-		1459,	-- Arcane Intellect, level 8
-		130,	-- Slow Fall, level 9
-	};
-	WARLOCK = {
-		686,	-- Start with Shadow Bolt
-		172,	-- Corruption, level 2
-		688,	-- Summon Imp, level 3
-		104773,	-- Unending Resolve, level 4
-		5782,	-- Fear, level 5
-		nil,	-- 
-		6201,	-- Create Healthstone, level 7
-		755,	-- Health Funnel, level 8
-		234153,	-- Drain Life, level 9
-	};
-	MONK = {
-		100780,	-- Start with Tiger Palm
-		100784,	-- Blackout Kick, level 2
-		109132,	-- Roll, level 3
-		116670,	-- Vivify, level 4
-		117952,	-- Crackling Jade Lightning, level 5
-		119381,	-- Leg Sweep, level 6
-		101546,	-- Spinning Crane Kick, level 7
-		322101,	-- Expel Harm, level 8
-		nil,	-- 328669, Roll Rank 2, level 9
-	};
-	DRUID = {
-		5176,	-- Start with Wrath
-		8921,	-- Moonfire, level 2
-		8936,	-- Regrowth, level 3
-		339,	-- Entangling Roots, level 4
-		nil,	-- 5221, Shred, level 5
-		1850,	-- Dash, level 6
-		nil,	-- 22568, Ferocious Bite, level 7
-		nil,	-- 33917, Mangle, level 8
-		nil,	-- 326646, Moonfire Rank 2, level 9
-	};
-}
 
 local function moveUp(self)
 	self:ClearAllPoints()
@@ -166,7 +50,7 @@ function script:GetActionButtonBySpellID(helper, spellID)
 		end
 	end
 	-- backup for stance bars
-	for i = 1, 10 do
+	for i = 1, GetNumShapeshiftForms() do
 		local btn =  _G["BT4StanceButton"..i]
 		local icon, isActive, isCastable, sID = GetShapeshiftFormInfo(btn:GetID())
 		if (sID == spellID) then
@@ -210,117 +94,6 @@ function script:FindEmptyButton()
 	end
 end
 
-local arrowDirections = {"UP", "LEFT", "RIGHT", "DOWN"}
-function script:ShowPointerArrow(frame, direction)
-	--Look for previous arrow and Hide
-	for i = 1, #arrowDirections do
-		local arrow1 = frame["Arrow_"..arrowDirections[i]..1]
-		local arrow2 = frame["Arrow_"..arrowDirections[i]..2]
-		if arrow1 then
-			arrow1:Hide()
-			--arrow1.Anim:Stop()
-			arrow2:Hide()
-			--arrow2.Anim:Stop()
-			if frame.AnimDelayTimer then frame.AnimDelayTimer:Cancel() end
-		end
-	end
-	-- -- Show the desired arrow
-	local arrow1 = frame["Arrow_"..direction..1]
-	local arrow2 = frame["Arrow_"..direction..2]
-	local point = (direction == "UP") and "TOP" or (direction == "DOWN") and "BOTTOM" or direction
-	arrow1:ClearAllPoints()
-	arrow2:ClearAllPoints()
-	local offsetX = (direction == "UP" or direction == "DOWN") and 0 or NegateIf(25, direction == "RIGHT")
-	local offsetY = (direction == "LEFT" or direction == "RIGHT") and 0 or NegateIf(25, direction == "DOWN")
-	arrow1:SetPoint(LUI.Opposites[point], frame, point, offsetX, offsetY)
-	-- arrow2:SetPoint(point, arrow2:GetParent(), LUI.Opposites[point])
-	arrow1:Show();
-	arrow1.Anim:Play();
-	-- --Second arrow starts half way through tx he first arrow's animation (1 second)
-	-- frame.AnimDelayTimer = C_Timer.NewTimer(0.5, function()
-	-- 	arrow2:Show();
-	-- 	arrow2.Anim:Play()
-	-- end)
-end
-
-local MicroButtons = {
-	Character = "LUIMicromenu_Player",
-	Spellbook = "LUIMicromenu_Spellbook",
-	Talents = "LUIMicromenu_Talents",
-	Achievement = "LUIMicromenu_Achievements",
-	QuestLog = "LUIMicromenu_Quests",
-	Guild = "LUIMicromenu_Guild",
-	LFD = "LUIMicromenu_LFG",
-	EJ = "LUIMicromenu_EJ",
-	Collections = "LUIMicromenu_Collections",
-	Bags = "LUIMicromenu_Bags",
-}
-
-function script:ShouldReAnchorPointer(frame)
-	local anchor
-	local text = frame.Content.Text:GetText()
-	if not frame:IsShown() and text then frame:Show() end
-	if text == _G.NPEV2_SPELLBOOK_TUTORIAL or text:find(NPEV2_SPELLBOOK_ADD_SPELL:gsub("%%s", "")) then
-		anchor = MicroButtons.Spellbook
-		-- HACK: Spellbook warning would always appear on reload.-
-		if not script:ShowSpellbook() then
-			frame:Hide()
-			-- This function isn't always called on level up, make sure to show it if needed.
-			script:RegisterEvent("PLAYER_LEVEL_CHANGED", function()
-				script:ShouldReAnchorPointer(frame)
-			end)
-			return
-		end
-	elseif text == format(_G.NPEV2_SHOW_BAGS, _G.TutorialHelper:GetBagBinding()) then
-		anchor = MicroButtons.Bags
-	elseif text == _G.NPEV2_LFD_INTRO then
-		anchor = MicroButtons.LFG
-		-- Make sure warning is only visible while you're on Exile Reach
-		if WorldMapFrame:GetMapID() ~= 1409 then
-			frame:Hide()
-			return
-		end
-	elseif text == _G.NPEV2_MOUNT_TUTORIAL_P2_NEW_MOUNT_ADDED then
-		anchor = MicroButtons.Collections
-	else
-		return
-	end
-	frame.currentTarget = _G[anchor]
-	frame:ClearAllPoints()
-	frame:SetPoint("TOP", anchor, "BOTTOM", 0, -100)
-	script:ShowPointerArrow(frame, "UP")
-end
-
--- Make sure the New Player Experience frame is pointing to the right button.
-function script:SetPointerFrame()
-	script:SecureHook(_G.TutorialPointerFrame, "Show", function(table, content, direction, anchorFrame, x, y, opposite)
-		if Micromenu and Micromenu:IsEnabled() then
-			local newPointer = anchorFrame.currentNPEPointer
-			script:ShouldReAnchorPointer(newPointer)
-		end
-	end)
-	-- If the frames already exist, hook will not be called so we have to check manually.
-	for i = 1, 3 do
-		local frame = _G["NPE_PointerFrame_"..i]
-		if frame then
-			script:ShouldReAnchorPointer(frame)
-		end
-	end
-end
-
-function script:ShowSpellbook()
-	local LevelUpTutorial_spellIDlookUp = _G.TutorialHelper:FilterByClass(TutorialData.LevelAbilitiesTable);
-	local playerLevel = UnitLevel("player");
-	for startLevel = 1, playerLevel do
-		local spellID = LevelUpTutorial_spellIDlookUp[startLevel];
-
-		local button = _G.TutorialHelper:GetActionButtonBySpellID(spellID);
-		if not button then
-			return true
-		end
-	end
-end
-
 function script:ADDON_LOADED(event, addon)
 	if addon == "Blizzard_NewPlayerExperience" then
 		if IsAddOnLoaded("Bartender4") then self.BT4 = true end
@@ -331,7 +104,6 @@ function script:ADDON_LOADED(event, addon)
 		end
 		
 		script:SetTutorialFrames()
-		script:SetPointerFrame()
 	end
 end
 
