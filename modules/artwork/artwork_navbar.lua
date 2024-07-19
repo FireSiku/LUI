10fb16e35096ee16597ab74af36373e6735fdf08
FireSiku <chaoslux@gmail.com>
2024-07-19 06:58:28 -0400

    Artwork: Added the NavBar, which had its anchoring all redone and using TexCoords.

--- modules/artwork/artwork_navbar.lua
+++ modules/artwork/artwork_navbar.lua
@@ -7,49 +7,130 @@ local LUI = select(2, ...)
 
 ---@class LUI.Artwork : LUIModule
 local module = LUI:GetModule("Artwork")
-local db
+
+local TEX_DIR = [[Interface\AddOns\LUI\media\templates\v4\]]
+local OLD_DIR = [[Interface\AddOns\LUI\media\templates\v3\]]
+local ANIM_DURATION = 0.5
+local ALPHA = 0.7
 
 -- ####################################################################################################################
 -- ##### Module Functions #############################################################################################
 -- ####################################################################################################################
+local function SetFrameBackdrop(frame, fileName)
+	if not frame then LUI:Print("frame not found:", fileName) end
+	frame:SetBackdrop({
+		bgFile = OLD_DIR..fileName, edgeSize = 1,
+		edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border",
+	})
+	frame:SetBackdropBorderColor(0,0,0,0)
+end
 
 function module:CreateNavBar()
-	db = self.db.profile
+	local topBackground = CreateFrame("Frame", "LUIArtwork_NavBarTopBackground", UIParent, "BackdropTemplate")
+	topBackground:SetSize(1024, 1024)
+	topBackground:SetFrameStrata("BACKGROUND")
+	topBackground:SetPoint("TOP", UIParent, "TOP", 17, -116)
+	SetFrameBackdrop(topBackground, "top")
+	topBackground:SetAlpha(ALPHA)
+
+	local centerBackground = CreateFrame("Frame", "LUIArtwork_NavBarCenterBackground", UIParent, "BackdropTemplate")
+	centerBackground:SetSize(1035, 1024)
+	centerBackground:SetFrameStrata("BACKGROUND")
+	centerBackground:SetPoint("TOP", UIParent, "TOP", 17, -117)
+	SetFrameBackdrop(centerBackground, "top_back_complete")
+	--SetFrameBackdrop(centerBackground, "top_back")
+	centerBackground:SetAlpha(ALPHA)
+
+	topBackground:SetFrameLevel(centerBackground:GetFrameLevel() + 1)
+	module.NavBar = topBackground
+
+	local r, g, b = LUI:GetClassColor("MAGE")
+	--topBackground:SetBackdropColor(r, g, b)
+	centerBackground:SetBackdropColor(r, g, b)
+
+	module:CreateNavButton("Chat", "left2", -164, -7)
+	module:CreateNavButton("Tps", "left1", -88, -7)
+	module:CreateNavButton("Dps", "right1", 58, -7)
+	module:CreateNavButton("Raid", "right2", 135, -7)
+end
+
+--- Create the NavBar buttons.
+---@param side "left2"|"left1"|"right1"|"right2"
+function module:CreateNavButton(kind, side, x, y)
+	local db = module.db.profile.LUITextures
+	local isWide = (side == "left2" or side == "right2")
 
 
+	local clicker = CreateFrame("Button", "LUIArtwork_NavBar"..side.."Clicker", UIParent)
+	clicker:SetSize(isWide and 70 or 63, 30)
+	clicker:SetFrameStrata("LOW")
+	--clicker:SetPoint("CENTER", button, "CENTER", isWide and -5 or 0, -12)
+	clicker:SetPoint("TOP", module.NavBar, "TOP", x, y)
+	clicker:SetAlpha(1)
+
+	local tex = clicker:CreateTexture(nil, "ARTWORK")
+	if isWide then
+		tex:SetSize(74, 25)
+	else
+		tex:SetSize(60, 24)
+	end
+	tex:SetPoint("TOP", module.NavBar, "TOP", x, y)
+	tex:SetTexture(OLD_DIR.."button_"..side)
+	tex:SetTexCoord(LUI:GetCoordAtlas("nav_button_"..side))
+
+	-- Animations
+	local alphaIn = tex:CreateAnimationGroup()
+	local a1 = alphaIn:CreateAnimation("Alpha")
+	a1:SetFromAlpha(0)
+	a1:SetToAlpha(ALPHA)
+	a1:SetDuration(ANIM_DURATION)
+	alphaIn:SetScript("OnFinished", function() tex:SetAlpha(ALPHA) end)
+
+	local alphaOut = tex:CreateAnimationGroup()
+	local a2 = alphaOut:CreateAnimation("Alpha")
+	a2:SetFromAlpha(ALPHA)
+	a2:SetToAlpha(0)
+	a2:SetDuration(ANIM_DURATION)
+	alphaOut:SetScript("OnFinished", function() tex:SetAlpha(0) end)
+
+	-- Hover animation
+	local hover = clicker:CreateTexture(nil, "ARTWORK")
+	if isWide then
+		hover:SetSize(74, 25)
+	else
+		hover:SetSize(60, 24)
+	end
+	hover:SetPoint("TOP", module.NavBar, "TOP", x, y)
+	hover:SetTexture(OLD_DIR.."button_"..side.."_hover")
+	hover:SetTexCoord(LUI:GetCoordAtlas("nav_button_"..side))
+	hover:SetAlpha(0)
+
+	clicker:RegisterForClicks("AnyUp")
+	clicker:SetScript("OnEnter", function() hover:SetAlpha(ALPHA) end)
+	clicker:SetScript("OnLeave", function() hover:SetAlpha(0) end)
+	clicker:SetScript("OnClick", function()
+		---@TODO: Make these SecureActions
+		if tex:GetAlpha() == 0 and not alphaIn:IsPlaying() then
+			LUI:Print(kind.."Click True")
+			db[kind].IsShown = true
+			alphaIn:Play()
+		elseif not alphaOut:IsPlaying() then
+			LUI:Print("Click False")
+			LUI:Print(kind.."Click True")
+			db[kind].IsShown = false
+			alphaOut:Play()
+		end
+	end)
+
+	local r, g, b = LUI:GetClassColor("MAGE")
+	tex: SetVertexColor(r, g, b)
+	hover:SetVertexColor(r, g, b)
 end
 
 -- ####################################################################################################################
 -- ##### Snippets of Old Code #############################################################################################
 -- ####################################################################################################################
 --[[ 
-
-	local function SetFrameBackdrop(frame, fileName)
-		if not frame then LUI:Print("frame not found:", fileName) end
-		frame.Backdrop = {
-			bgFile = directory..fileName,
-			edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border",
-			edgeSize = 1,
-			insets = {left = 0, right = 0, top = 0, bottom = 0}
-		}
-		frame:SetBackdrop(frame.Backdrop)
-		frame:SetBackdropBorderColor(0,0,0,0)
-	end
-
-	LUI.Navi.TopButtonBackground = LUI:CreateMeAFrame("Frame", nil, UIParent, 1024, 1024, 1, "BACKGROUND", 1, "TOP", UIParent, "TOP", 17, -16, 1)
-	SetFrameBackdrop(LUI.Navi.TopButtonBackground, "top")
-	LUI.Navi.TopButtonBackground:Show()
-
-	LUI.Navi.CenterBackground = LUI:CreateMeAFrame("Frame", nil, UIParent, 1035, 1024, 1, "BACKGROUND", 0, "TOP", UIParent, "TOP", 17, -17, 1)
-	SetFrameBackdrop(LUI.Navi.CenterBackground, "top_back")
-	LUI.Navi.CenterBackground:SetBackdropColor(unpack(color_top))
-	LUI.Navi.CenterBackground:Show()
-
-	LUI.Navi.CenterBackgroundAlternative = LUI:CreateMeAFrame("Frame", nil, UIParent, 1035, 1024, 1, "BACKGROUND", 0, "TOP", UIParent, "TOP", 17, -17, 1)
-	SetFrameBackdrop(LUI.Navi.CenterBackgroundAlternative, "top_back_complete")
-	LUI.Navi.CenterBackgroundAlternative:SetBackdropColor(unpack(color_top))
-	LUI.Navi.CenterBackgroundAlternative:Hide()
-
 	-- Chat button.
 	LUI.Navi.Chat = LUI:CreateMeAFrame("Frame", nil, LUI.Orb, 126, 120, 1, "LOW", 1, "LEFT", LUI.Orb, "LEFT", -176, 49, 0)
 	LUI.Navi.Chat:SetBackdrop({
