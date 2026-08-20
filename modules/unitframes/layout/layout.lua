a49be75893f882fb4782c5144657984f9f159058
Pahn1988 <74673519+Pahn1988@users.noreply.github.com>
2026-08-20 21:26:02 +0200

    Revert avoid duplicate target debuff refresh

--- modules/unitframes/layout/layout.lua
+++ modules/unitframes/layout/layout.lua
@@ -2181,7 +2181,7 @@ module.funcs = {
 		end
 
 		self.Debuffs:SetUnit(unit)
-
+		self.Debuffs:UpdateAllAuras()
 		for i = 1, #self.Debuffs do
 			local button = self.Debuffs[i]
 			if button and button:IsShown() then
