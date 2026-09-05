ce808fbed4fa132c8ffee2793871a89775e904b7
Pahn <74673519+Pahn1988@users.noreply.github.com>
2026-09-05 15:31:33 +0200

    Bump embedded oUF version to 14.0.3
    Update the embedded oUF version references and compatibility documentation to 14.0.3.

--- api/oUF13/ouf.lua
+++ api/oUF13/ouf.lua
@@ -1,6 +1,6 @@
 local parent, ns = ...
 local global = C_AddOns.GetAddOnMetadata(parent, 'X-oUF')
-local _VERSION = '14.0.2'
+local _VERSION = '14.0.3'
 if(_VERSION:find('project%-version')) then
 	_VERSION = 'devel'
 end
